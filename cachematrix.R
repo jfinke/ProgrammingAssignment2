99bd8e60f16fc7d4b2d1b0a96938ff865e0de067
Jeremy Finke <jeremy.finke@gmail.com>
2014-10-21 18:43:07 -0500

    Change first comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions work together to create a special caching algorithm for 
+## caching the inverse of a matrix
 
 ## Write a short comment describing this function
 
